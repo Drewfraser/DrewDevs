e4a975ea784868ef15db578019376559a0909213
Drewfraser <95208170+Drewfraser@users.noreply.github.com>
2025-02-20 11:13:07 +1300

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,288 @@
+const logotext = "DrewDevs";
+const meta = {
+  title: "Drew Frasers Devs",
+  description:
+    "I’m Drew Fraser Full stack devloper,currently working in New Zealand",
+};
+
+const introdata = {
+  title: "I’m Drew Fraser",
+  animated: {
+    first: "I love Apple iOS",
+    second: "I am enjoying learning about cybersecurity",
+    third: "I am studying full stack engineering",
+  },
+  description:
+    "Experienced professional with a diverse background in laboratory operations, event coordination, lifeguard services, and administrative support. Currently working as a Laboratory Technician at Hill Labs, ensuring compliance with lab protocols and quality standards. Skilled in event planning and setup through my role as an Operations Assistant with New Plymouth District Council. Over seven years as a volunteer lifeguard with Surf Life Saving New Zealand, demonstrating leadership and emergency response capabilities. Previous experience includes specimen handling at Awanui Group and legal support roles, showcasing strong organizational, customer service, and data management skills.",
+
+  //edited this
+  your_img_url:
+    "https://media-hosting.imagekit.io//3fe74d40ccc04cfe/62B7ED81-D813-4F89-8F4E-6A0F8C92BB0C.jpg?Expires=1834362252&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=Sr0eHZ9nCgigxGWq3jipYOfFvgdKQocGcSM76jYAr8HtUdhWmVq07wTJZC-dZu53H1p1dWDCUg4ClQf7wj9KJUOPgMYoBzfruSr~N4MJqxdxoCkYkf-Ief7SSAxKk0dxh7y4A5ezVzkxqfqb34CD0UWtwwrDkQfahIN4zRTLWRej3R5tE9W4eLvu5rP5UWUOW-LIEo6gSqdEp4yBqAXZxi0OIqPD4rOpmAM93p6tIEKsEjctL4W13LuCAceHNPtOwhdXwSUmckVHQVa3HC3CEbX8T8albUHZWnmbQl9U3454plO5vHTzOok8pzHusJtrrXV6lFc6kH2Jdy18BpqvQA__",
+};
+
+const dataabout = {
+  title: "A little bit about my self",
+  aboutme:
+    "Experienced professional with a diverse background in laboratory operations, event coordination, lifeguard services, and administrative support. Currently working as a Laboratory Technician at Hill Labs, ensuring compliance with lab protocols and quality standards. Skilled in event planning and setup through my role as an Operations Assistant with New Plymouth District Council. Over seven years as a volunteer lifeguard with Surf Life Saving New Zealand, demonstrating leadership and emergency response capabilities. Previous experience includes specimen handling at Awanui Group and legal support roles, showcasing strong organizational, customer service, and data management skills.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Laboratory Technician",
+    where: "Hill Labs, Hamiltion",
+    date: "Jan 2025 - Present",
+  },
+  {
+    jobtitle: "Operations Assistant",
+    where: "New Plymouth District Council",
+    date: "Aug 2023 - Present",
+  },
+  {
+    jobtitle: "Volenteer Surf Lifeguard",
+    where: "Fitzroy Surf Life Saving Club",
+    date: "Nov 2019 - Present",
+  },
+  {
+    jobtitle: "Specimen Services Lab Technician",
+    where: "Awanui Group",
+    date: "May 2024 - Nov 2024, 7mos",
+  },
+  {
+    jobtitle: "Pool Lifeguard",
+    where: "New Plymouth District Council",
+    date: "Aug 2023 - Apr 2024, 2yrs 9 mos",
+  },
+  {
+    jobtitle: "Legal Support Assistant",
+    where: "Nina Laird Barrister and Solicitor",
+    date: "Nov 2023 - Jan 2024, 3mos",
+  },
+  {
+    jobtitle: "Client Experience and Admistration Support",
+    where: "Govett Quilliam Law ",
+    date: "Jun 2022 - Nov 2023, 1yr 7mos",
+  },
+];
+
+const skills = [
+  {
+    name: "CyberSecurity",
+    value: 90,
+  },
+  {
+    name: "Web development",
+    value: 90,
+  },
+  {
+    name: "Infomation Technology",
+    value: 90,
+  },
+  {
+    name: "Mathmatics",
+    value: 90,
+  },
+  {
+    name: "HTML 5",
+    value: 85,
+  },
+  {
+    name: "CSS",
+    value: 85,
+  },
+  {
+    name: "Java",
+    value: 85,
+  },
+  {
+    name: "Phyton",
+    value: 80,
+  },
+  {
+    name: "C#",
+    value: 80,
+  },
+  {
+    name: "React Js",
+    value: 80,
+  },
+  {
+    name: "German",
+    value: 80,
+  },
+  {
+    name: "Te Reo (Maori)",
+    value: 80,
+  },
+  {
+    name: "Web design",
+    value: 70,
+  },
+  {
+    name: "Computer science",
+    value: 60,
+  },
+  {
+    name: "SQL",
+    value: 60,
+  },
+  {
+    name: "Artificial Intellingence",
+    value: 40,
+  },
+  {
+    name: "DevOps",
+    value: 30,
+  },
+  {
+    name: "Bash",
+    value: 30,
+  },
+  {
+    name: "Cloud Computing",
+    value: 30,
+  },
+  {
+    name: "Data Science",
+    value: 30,
+  },
+  {
+    name: "C++",
+    value: 30,
+  },
+];
+
+const services = [
+  {
+    title: "Full Stack Engineering, Codeademy, Current Study",
+    description:
+      "I will learn all the major technologies essential for a full-stack developer, gaining a foundation in JavaScript programming while mastering front-end technologies like HTML, CSS, and React, and building secure back-ends with Node/Express and PostgreSQL. This course will prepare me for technical interviews by developing skills such as setting up a development environment, version-controlling work with Git/GitHub, writing client-side and server-side JavaScript code, creating static and responsive websites with HTML/CSS, using the React framework with Redux, building back-ends with Node/Express, designing PostgreSQL databases, connecting front-end and back-end for full-stack PERN applications, implementing security best practices to prevent attacks, and successfully passing JavaScript whiteboard interviews. This comprehensive training will provide a strong foundation for a full-stack engineering career.",
+  },
+  {
+    title:
+      "New Zealand Certifcate in Bussiness (Accounting Support Services), Level 4, Current Study",
+    description:
+      "This qualification will equip me with the knowledge to perform a wide range of accounting support services, preparing me for various roles that contribute to achieving a business entity's objectives. It will enable me to provide accounting support under broad guidance, while aligning with ngā kaupapa o te Tiriti o Waitangi (the principles of the Treaty of Waitangi) and working within a multi-cultural environment.",
+  },
+  {
+    title:
+      "New Zealand Dipolma in Web Design and Development, Level 5, Whitecliffe College, Mar 2024",
+    description:
+      "I have completed a program designed to prepare for entry-level roles as a web developer. Through this program, I gained practical IT and professional skills essential for success in an IT career. I can now build complete web applications, following the entire development process from start to finish, using a content management system. Additionally, I developed a broad set of internationally relevant IT skills and the ability to work both independently and collaboratively while adhering to professional standards and practices. I have completed a program designed to prepare for entry-level roles as a web developer. Through this program, I gained practical IT and professional skills essential for success in an IT career. I can now build complete web applications, following the entire development process from start to finish, using a content management system. Additionally, I developed a broad set of internationally relevant IT skills and the ability to work both independently and collaboratively while adhering to professional standards and practices.",
+  },
+  {
+    title: "Fundamentals of Cybersecurity, Codeademy, Dec 2024",
+    description:
+      "During this course, I gained insight into how hackers infiltrate systems, the dangers of ransomware, and its potential impact on both individuals and large organizations. Guided by the principles of CompTIA’s Security+ Certification, I explored the techniques used by attackers, identified the different types of threat actors, and learned proactive strategies to enhance security and mitigate risks.",
+  },
+  {
+    title: "New Zealand National Certificate of Educational Achievement,",
+    description:
+      "I have achieved a New Zealand Diploma in Web Development and Design (Level 5) from Whitecliffe College, which I completed on 19 April 2024, and I also hold the National Certificate of Educational Achievement (NCEA) Levels 1, 2, and 3 with Merit from the Ministry of Education, earning University Entrance on 9 December 2021. My qualifications are supported by course endorsements in subjects such as Biology, Chemistry, English, Mathematics, Physics, Accounting, Science, and Visual Arts, with achievements ranging from Merit to Excellence. Throughout my studies, I have developed expertise in areas like financial calculations, statistical interpretation, spectroscopic data analysis, thermochemical principles, evolutionary processes, mechanical systems, and genetic variation. I have honed my English skills by producing crafted and coherent writing, responding critically to texts, and making significant connections across various forms of literature. My strong foundation also includes Accounting, Mathematics, Statistics, Health Studies, and Recreation and Sport, where I’ve gained experience in linear programming, probability methods, financial decision-making, and first aid certifications. I have conducted practical investigations in science, explored advanced media and drawing methods in Visual Arts, and taken on leadership roles in safety and emergency response in aquatic environments. This diverse educational background reflects my well-rounded capabilities and my commitment to excellence across various disciplines.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "drewdevs@icloud.com",
+  YOUR_FONE: "1234567890",
+  description:
+    "Feel free to reach out! I’m always open to discussing opportunities, sharing ideas, or answering any questions you may have. You can contact me via email or phone, and I’ll respond promptly. I look forward to connecting with you! ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/Drewfraser",
+  facebook: "https://facebook.com",
+  linkedin: "https://www.linkedin.com/in/drewfraser03/",
+  twitter: "https://x.com/drewfraser9",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  services,
+  skills,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
